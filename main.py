3e479f428fea2ab2c8518056e34b7ee14366de0a
Sea Rex <wheeler_harry@icloud.com>
2025-01-30 19:40:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Let's create a fact file on you!")
 firstname = input("Please enter your first name: ")
 lastname = input("Please enter your last name: ")
+age = int(input("Please enter your current age: "))
